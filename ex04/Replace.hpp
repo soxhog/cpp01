057e24f66f239a65aece8b34e96b6f124957f8fa
Shogo Kagiya <skagiya@c1r17s1.42tokyo.jp>
2021-10-09 16:59:08 +0900

    add put_err

---
+++ ex04/Replace.hpp
@@ -0,0 +1,16 @@
+#ifndef REPLACE_HPP
+#define REPLACE_HPP
+
+# include <string>
+# include <iostream>
+
+// ARGS
+
+# define MUST_ARGS_NUM 4
+
+// ERROR 
+
+# define ERROR 1
+# define ERR_ARGS_NUM 2
+
+#endif /* REPLACE_HPP */
--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,7 +1,37 @@
-# include <string>
-# include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skagiya <skagiya@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/09 16:48:44 by skagiya           #+#    #+#             */
+/*   Updated: 2021/10/09 16:58:43 by skagiya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Replace.hpp"
+
+void	put_err(int err_num)
+{
+	std::cout << "Error" << std::endl;
+	switch (err_num)
+	{
+		case MUST_ARGS_NUM:
+			std::cout << "Wrong Args Numbers" << std::endl;
+			break;
+		
+		default:
+			break;
+	}
+}
 
 int	main(int argc, char **argv)
 {
+	if (argc != MUST_ARGS_NUM)
+	{
+		put_err(ERR_ARGS_NUM);
+		return (ERROR);
+	}
 	return (0);
 }
